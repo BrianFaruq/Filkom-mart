5260123c7942e1c21c630aee11084d6751eb9fe4
BrianFaruq <brian.faruqw16@gmail.com>
2026-03-14 20:48:28 +0700

    bug fix

--- Transaction.java
+++ Transaction.java
@@ -2,14 +2,44 @@ public class Transaction {
     private String transactionId;
     private Product[] items;
     private int totalItems;
-    public double processSale(){
-        return 0;
+
+    // constructor untuk membuat transaksi
+    public Transaction(String transactionId) {
+        this.transactionId = transactionId;
+        this.items = new Product[50];
+        this.totalItems = 0;
     }
-    public void addItem(Product item){
 
+    // menghitung total harga setelah diskon semua item
+    public double processSale() {
+        double total = 0;
+
+        for (int i = 0; i < totalItems; i++) {
+            Product p = items[i];
+
+            double discount = p.calculateDiscount();
+            double finalPrice = p.getPrice() - (p.getPrice() * discount);
+
+            total += finalPrice;
+        }
+
+        return total;
     }
 
-    public void addItem(Product item, int quantity){
+    // menambahkan satu produk ke transaksi
+    public void addItem(Product item) {
+        if (totalItems < items.length) {
+            items[totalItems] = item;
+            totalItems++;
+        } else {
+            System.out.println("Keranjang penuh");
+        }
+    }
 
+    // method overloading untuk menambahkan beberapa produk sekaligus
+    public void addItem(Product item, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            addItem(item);
+        }
     }
 }
